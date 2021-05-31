66ff4053bcd7d1e89ec6ba2f099acff5fe5d5e59
Amaury <amauryverschooren@icloud.com>
2021-05-31 21:45:58 +0100

    final adjustments

--- Game_Engine/Game_Engine.py
+++ Game_Engine/Game_Engine.py
@@ -23,104 +23,6 @@ client = mqtt.Client(client_id="Game_engine", clean_session=True, userdata=None,
 
 
 
-class Ball:
-	def __init__(self, xPos, yPos):
-		self.positionX = xPos
-		self.positionY = yPos
-		self.x_heading = 1
-		self.y_heading = 1
-	def send_position(self):
-		client.publish(ball_topic, str(self.positionX) + "; " + str(self.positionY))
-	def update_position(self):
-		self.positionX = self.positionX + (2 * self.x_heading)
-		self.positionY = self.positionY + (2 * self.y_heading)
-		if self.positionX >= canvas_width -20:
-			self.x_heading = -1
-		if self.positionY >= canvas_height -20:
-			self.y_heading = -1
-		if self.positionX <= 0:
-			self.x_heading = 1
-		if self.positionY <= 0:
-			self.y_heading = 1
-		print(str(self.positionX) + " - " + str(self.positionY))
-		self.check_collision_pad()
-	def check_collision_pad(self):
-		if self.positionX + 20 >= canvas_width:
-			Racket1.score += Racket1.temp_score
-			Racket2.temp_score = 0
-			client.publish(score_topic, "PLAYER_"+str(Racket2.playerNumber)+"; SCORE=" + str(Racket2.score))
-			client.publish(score_topic, "PLAYER_"+str(Racket1.playerNumber)+"; SCORE=" + str(Racket1.score))
-			self.new_round()
-		elif self.positionX <= 0:
-			Racket2.score += Racket2.temp_score
-			Racket1.temp_score = 0
-			client.publish(score_topic, "PLAYER_"+str(Racket2.playerNumber)+"; SCORE=" + str(Racket2.score))
-			client.publish(score_topic, "PLAYER_"+str(Racket1.playerNumber)+"; SCORE=" + str(Racket1.score))
-			self.new_round()
-		elif self.positionX + 20 >= (canvas_width - (20 + racket_dimensions[0])):
-			if (self.positionY + 20) < (Racket2.yPosition + racket_dimensions[1]) and self.positionY > Racket2.yPosition:
-				if (self.positionX +20) <= (canvas_width - 20):
-					print("trigger collision pad")
-					self.x_heading = -1
-					Racket2.temp_score += 5
-					print("dit is de score van speler 2: " + str(Racket2.score))
-					client.publish(score_topic, "PLAYER_"+str(Racket2.playerNumber)+"; SCORE=" + str(Racket2.temp_score))
-		elif self.positionX <= (20 + racket_dimensions[0]):
-			if (self.positionY + 20) < (Racket1.yPosition + racket_dimensions[1]) and self.positionY > Racket1.yPosition:
-				if self.positionX >= 20:
-					print("trigger collision pad")
-					self.x_heading = 1
-					Racket1.temp_score += 5
-					print("dit is de score van speler 1: " + str(Racket1.score))
-					client.publish(score_topic, "PLAYER_"+str(Racket1.playerNumber)+"; SCORE=" + str(Racket1.temp_score))
-		self.send_position()
-	def new_round(self):
-		print("nieuwe ronde")
-
-		global rounds
-		global end_game_flag
-		rounds += 1 if rounds < 11 else 0
-		print(rounds)
-		if rounds == 2:
-			end_game_flag = True
-		self.positionX = ((canvas_width/2)-(ball_dimensions[0]/2))
-		self.positionY = ((canvas_height/2)-(ball_dimensions[1]/2))
-
-class Racket:
-	def __init__(self, xPos,yPos,number):
-		self.playerNumber = number
-		self.xPosition = xPos
-		self.yPosition = yPos
-		self.score = 0
-		self.temp_score = 0
-		self.racketVelocity = 1
-		self.high_speed_triggered = False
-	def update_position(self, input_number):
-		if self.yPosition + input_number < 0 or self.yPosition + racket_dimensions[1] + input_number > canvas_height:
-			self.yPosition = self.yPosition
-		else:
-			self.yPosition += (input_number * self.racketVelocity)
-			print("position racket: " + str(self.playerNumber) + " updated!")
-			self.send_position()
-	def update_velocity(self):
-		if self.high_speed_triggered == False:
-			self.racketVelocity = 2
-		else:
-			self.racketVelocity = 1
-	def send_position(self):
-		client.publish(racket_topic, "RACKET_"+str(self.playerNumber)+"; POSITION=" + str(self.yPosition))
-		
-		
-
-#aanmaken bal en pads
-Racket1 = Racket(canvas_width - canvas_width +1, canvas_height/2-racket_dimensions[1]/2,1)
-Racket2 = Racket(canvas_width - 1, canvas_height/2-racket_dimensions[1]/2,2)
-Ball = Ball(((canvas_width/2)-(ball_dimensions[0]/2)), ((canvas_height/2)-(ball_dimensions[1]/2)))
-
-
-
-
-	
 def on_connect(client, userdata, flags, rc):
 	if rc==0:
 		print("connected OK Returned code=",rc)
@@ -176,11 +78,116 @@ def on_message(client, userdata, msg):
 	elif msg.topic == "game/state":
 		if msg.payload == "CONTROLLER=GAME_STARTED":
 			print("game started from controller")
-			client.publish(state_topic, "GAME_STARTED")
-			time.sleep(2)
-			trigger_leds()
+			client.publish(state_topic, "GAME_STARTED", qos=0)
+			time.sleep(7)
 			ball_flag = 1
-			# threading.Timer(2, game_loop()).start()
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("213.119.34.109", 1888, 60)
+client.subscribe([(hello_topic,0),(racket_topic,0),(control_topic,0), (state_topic,0)])
+client.loop_start()
+
+class Ball:
+	def __init__(self, xPos, yPos):
+		self.positionX = xPos
+		self.positionY = yPos
+		self.x_heading = 1
+		self.y_heading = 1
+	def send_position(self):
+		client.publish(ball_topic, str(self.positionX) + "; " + str(self.positionY))
+	def update_position(self):
+		self.positionX = self.positionX + (2 * self.x_heading)
+		self.positionY = self.positionY + (2 * self.y_heading)
+		if self.positionX >= canvas_width -20:
+			self.x_heading = -1
+		if self.positionY >= canvas_height -20:
+			self.y_heading = -1
+		if self.positionX <= 0:
+			self.x_heading = 1
+		if self.positionY <= 0:
+			self.y_heading = 1
+		print(str(self.positionX) + " - " + str(self.positionY))
+		self.check_collision_pad()
+	def check_collision_pad(self):
+		if self.positionX + 20 >= canvas_width:
+			Racket1.score += Racket1.temp_score
+			Racket2.temp_score = 0
+			client.publish(score_topic, "PLAYER_"+str(Racket2.playerNumber)+"; SCORE=" + str(Racket2.score))
+			client.publish(score_topic, "PLAYER_"+str(Racket1.playerNumber)+"; SCORE=" + str(Racket1.score))
+			self.new_round()
+		elif self.positionX <= 0:
+			Racket2.score += Racket2.temp_score
+			Racket1.temp_score = 0
+			client.publish(score_topic, "PLAYER_"+str(Racket2.playerNumber)+"; SCORE=" + str(Racket2.score))
+			client.publish(score_topic, "PLAYER_"+str(Racket1.playerNumber)+"; SCORE=" + str(Racket1.score))
+			self.new_round()
+		elif self.positionX + 20 >= (canvas_width - (20 + racket_dimensions[0])):
+			if (self.positionY + 20) < (Racket2.yPosition + racket_dimensions[1]) and self.positionY > Racket2.yPosition:
+				if (self.positionX +20) <= (canvas_width - 20):
+					print("trigger collision pad")
+					self.x_heading = -1
+					Racket2.temp_score += 5
+					print("dit is de score van speler 2: " + str(Racket2.score))
+					client.publish(score_topic, "PLAYER_"+str(Racket2.playerNumber)+"; SCORE=" + str(Racket2.temp_score))
+		elif self.positionX <= (20 + racket_dimensions[0]):
+			if (self.positionY + 20) < (Racket1.yPosition + racket_dimensions[1]) and self.positionY > Racket1.yPosition:
+				if self.positionX >= 20:
+					print("trigger collision pad")
+					self.x_heading = 1
+					Racket1.temp_score += 5
+					print("dit is de score van speler 1: " + str(Racket1.score))
+					client.publish(score_topic, "PLAYER_"+str(Racket1.playerNumber)+"; SCORE=" + str(Racket1.temp_score))
+		self.send_position()
+	def new_round(self):
+		print("nieuwe ronde")
+		client.publish(state_topic, "NEW_ROUND")
+		time.sleep(7)
+		global rounds
+		global end_game_flag
+		rounds += 1 if rounds < 11 else 0
+		print(rounds)
+		if rounds == 3:
+			end_game_flag = True
+		self.positionX = ((canvas_width/2)-(ball_dimensions[0]/2))
+		self.positionY = ((canvas_height/2)-(ball_dimensions[1]/2))
+
+class Racket:
+	def __init__(self, xPos,yPos,number):
+		self.playerNumber = number
+		self.xPosition = xPos
+		self.yPosition = yPos
+		self.score = 0
+		self.temp_score = 0
+		self.racketVelocity = 1
+		self.high_speed_triggered = False
+	def update_position(self, input_number):
+		if self.yPosition + input_number < 0 or self.yPosition + racket_dimensions[1] + input_number > canvas_height:
+			self.yPosition = self.yPosition
+		else:
+			self.yPosition += (input_number * self.racketVelocity)
+			print("position racket: " + str(self.playerNumber) + " updated!")
+			self.send_position()
+	def update_velocity(self):
+		if self.high_speed_triggered == False:
+			self.racketVelocity = 2
+		else:
+			self.racketVelocity = 1
+	def send_position(self):
+		client.publish(racket_topic, "RACKET_"+str(self.playerNumber)+"; POSITION=" + str(self.yPosition))
+		
+		
+
+#aanmaken bal en pads
+Racket1 = Racket(canvas_width - canvas_width +1, canvas_height/2-racket_dimensions[1]/2,1)
+Racket2 = Racket(canvas_width - 1, canvas_height/2-racket_dimensions[1]/2,2)
+Ball = Ball(((canvas_width/2)-(ball_dimensions[0]/2)), ((canvas_height/2)-(ball_dimensions[1]/2)))
+
+
+
+
+	
+
 			
 			
 def publish_hello(message):
@@ -195,20 +202,8 @@ def end_game():
 		client.publish(state_topic, "GAME_END; PLAYER_2")
 	else:
 		client.publish(state_topic, "GAME_END; GELIJK")
-def trigger_leds():
-	for	i in range(3):
-		client.publish(led_topic, "LED_ON")
-		print("led on")
-		time.sleep(1)
-		client.publish(led_topic, "LED_OFF")
-		print("led off")
-		time.sleep(1)
 
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("213.119.34.109", 1888, 60)
-client.subscribe([(hello_topic,0),(racket_topic,0),(control_topic,0), (state_topic,0)])
-client.loop_start()
+
 while ball_flag == 0:
 	time.sleep(0)
 	#print("Game ready")
